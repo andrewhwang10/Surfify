b981f693f6b3e34bf69d07b32616a8a02864fcf3
Andrew Hwang <andrewhwang10@gmail.com>
2018-03-08 00:02:05 -0800

    Add content script

--- background.js
+++ background.js
@@ -1,8 +1,16 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('window.html', {
-      'outerBounds': {
-        'width': 300,
-        'height': 200
-      }
-    });
+  chrome.app.window.create('window.html', {
+    'outerBounds': {
+      'width': 300,
+      'height': 200
+    }
   });
+  console.log("load");
+  chrome.tabs.sendMessage({text: 'report_back'}, doStuffWithDom);
+});
+
+
+// A function to use as callback
+function doStuffWithDom(domContent) {
+  console.log('I received the following DOM content:\n' + domContent);
+}
