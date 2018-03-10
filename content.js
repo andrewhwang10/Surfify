9ec3bbe6a4d4344e38fd86c4ed3a05fc738a4fcb
Andrew Hwang <andrewhwang10@gmail.com>
2018-03-09 23:53:53 -0800

    Add functionality for extension to search spotify

--- content.js
+++ content.js
@@ -2,4 +2,5 @@ var url = location.href;
 var title = document.getElementsByTagName("title")[0].innerHTML;
 
 let page = { url: location.href, title: document.getElementsByTagName("title")[0].innerHTML };
+
 chrome.runtime.sendMessage(page);
--- eventPage.js
+++ eventPage.js
@@ -1,40 +1,28 @@
-// let player = $("#player");
-// let uri = "https://open.spotify.com/embed?uri=";
-// player.attr("src", uri+"spotify:track:1g5AwXgekJ2ToKHw0Nw2p5");
-
 // Listens for page information
 const path = chrome.runtime.getURL("./stopwords.txt");
 const client_id = "bdd9c482459147cfab6e37b53bd12111";
 const client_secret = "0af638224b40476198c7ac3ee514b062";
 const spotifySearch = "https://api.spotify.com/v1/search";
 
+let token = '';
+
 getAuthorization();
-
-let file = new File([""], path);
-let stopWords = new Set();
-let reader = new FileReader();
-
-reader.onload = function(e) {
-  e.target.result
-  for (let i = 0; i < e.target.result.length; i++) {
-    stopWords.add(e.target.result[i]);
-  }
-}
-reader.readAsText(file);
-
 
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if(request !== undefined) {
-      console.log("TITLE: " + request.title);
-      let query = parseWords(request.title);
-      query = removeStopWords(query);
-      console.log(stopWords);
-      console.log("WORDS: " + query);
-      httpGetAsync(query[0]);
-    }      
+      getSongFromSpotify(request);
+    }     
 });
+
+function getSongFromSpotify(pageData) {
+  console.log("TITLE: " + pageData.title);
+  let query = parseWords(pageData.title);
+
+  console.log("WORDS: " + query);
+  makeSearchRequest(query);
+}
 
 // Parses words into array, removing punctuation and whitespace
 function parseWords(words) {
@@ -43,45 +31,57 @@ function parseWords(words) {
   return words;
 }
 
-function removeStopWords(words) {
-  for(let i = 0; i < words.length; i++) {
-    if(stopWords.has(words[i].toLowerCase())) {
-      words.splice(i, 1);
-      i--;
-    }
-  }
-  return words;
-}
-
-function httpGetAsync(parameters) {
+function makeSearchRequest(query) {
     var xmlHttp = new XMLHttpRequest();
-    var URL = "https://api.spotify.com/v1/search";
+    var parameters = "q=" + query[0];
+    for(let i = 1; i < query.length; i++) {
+      if(i > 1) {
+        break;
+      } else {
+        parameters += "%20" + query[i];
+      }
+    }
+    parameters += "&type=track&limit=1";
+    console.log(parameters);
+
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-          let object = xmlHttp.responseText;
-          console.log(object);
+          if(xmlHttp.response.tracks.items.length > 0) {
+            let object = xmlHttp.response.tracks.items[0].uri;
+            
+            let player = document.getElementById("player");
+            let uri = "https://open.spotify.com/embed?uri=";
+            player.setAttribute("src", uri+object);
+          } else {
+            console.log("There are no songs related to this webpage title");
+          }
         }
     }
    
-    xmlHttp.open("GET", URL, true); // true for asynchronous
-    xmlHttp.setRequestHeader("Authorization", "Bearer" + token);
-    var q = "q=" + parameters + "&type=track";
-    xmlHttp.send(q);
+    xmlHttp.open("GET", "https://api.spotify.com/v1/search?" + parameters, true); 
+    xmlHttp.responseType = "json";
+    xmlHttp.setRequestHeader("Authorization", "Bearer " + token);
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    
+    xmlHttp.send(null);
 }
 
 function getAuthorization() {
   var xmlHttp = new XMLHttpRequest();
-  var baseCode = btoa(client_id + client_secret);
+  var baseCode = btoa(client_id + ":" + client_secret);
   var params = "grant_type=client_credentials";
 
   xmlHttp.onreadystatechange = function() { 
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      token = xmlHttp.responseText;
+      token = xmlHttp.response.access_token;
       console.log(token);
     }
   }
 
   xmlHttp.open("POST", "https://accounts.spotify.com/api/token", true);
+  xmlHttp.responseType = "json";
   xmlHttp.setRequestHeader("Authorization", "Basic " + baseCode);
+  xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
   xmlHttp.send(params);
 }
+
